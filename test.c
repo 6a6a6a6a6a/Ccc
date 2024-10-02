a551f8b6eaec2b97c0040bf020b0add07dbee87d
6a6a6a6a6a <2980557072@qq.com>
2024-10-02 17:46:39 +0800

    first commit

---
+++ test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, isPrime;
+
+    printf("100以内的质数是: \n");
+    
+     i=3;
+    j=2;
+    isPrime=i/j;
+    printf("%d",isPrime);
+    
+    return 0;
+}
